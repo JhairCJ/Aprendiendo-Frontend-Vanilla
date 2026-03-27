e0f485d451f5846b364a2a2a8aa2f3379076a8e6
JhairCJ <jhaircjxd@gmail.com>
2026-03-27 11:17:24 -0400

    feat: add JS variable notes & a little handling of html

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+console.log(`hello`);
+
+document.getElementById("titulo").textContent = `Hello`;
+document.getElementById("texto").textContent = "this is a text.";
+let fullName = "Jhair Calicho";
+let age = 21;
+let isStudent = true;
+document.getElementById("fullName").textContent = `Your name is ${fullName}`;
+document.getElementById("age").textContent = `Your age is ${age} years old`
+document.getElementById("student").textContent = `You are a student: ${isStudent}`
