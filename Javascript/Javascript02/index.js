1640a6687f5e3426471c9379c59ad65ca6d2dff8
JhairCJ <jhaircjxd@gmail.com>
2026-03-28 12:53:31 -0400

    feat: add get data from username input and say hello to that username

---
+++ Javascript/Javascript02/index.js
@@ -0,0 +1,5 @@
+let username;
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("username").value;
+    document.getElementById("title").textContent = `Hello, ${username}`
+}
